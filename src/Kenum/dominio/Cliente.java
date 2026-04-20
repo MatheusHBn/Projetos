990cbf55cbc9cbd7af0741be48a84851ea342732
MatheusHBn18 <matheushbn123@gmail.com>
2026-04-20 11:59:34 -0300

    Projeto da faculdade ta tomando muito tempo, mas não da pra parar de estudar Java

--- src/Kenum/dominio/Cliente.java
+++ src/Kenum/dominio/Cliente.java
@@ -3,22 +3,22 @@ package Kenum.dominio;
 public class Cliente {
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
+    public enum TipoPagamento{
+        DEBITO, CREDITO
+    }
 
-    public Cliente(String nome, TipoCliente tipoCliente){
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString(){
-        return "Nome = " + nome + '\'' + "tipo = " + tipoCliente;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+        return "Cliente: "+ this.nome + "\n Tipo do Cliente: "+ this.tipoCliente.getNomeJuridico()  + "Tipo do cliente: " + this.tipoCliente.getVALOR() +
+                "\n Tipo do Pagamento: " + this.tipoPagamento +"\n---------";
     }
 }
+
+
--- src/Kenum/dominio/TipoCliente.java
+++ src/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private final int VALOR;
+    private String nomeJuridico;
+
+    TipoCliente(int VALOR, String nomeJuridico) {
+        this.VALOR = VALOR;
+        this.nomeJuridico = nomeJuridico;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNomeJuridico() {
+        return nomeJuridico;
+    }
 }
